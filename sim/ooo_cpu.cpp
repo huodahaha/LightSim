4358e702760cecdefc6cd79f522cb69edecceb25
Huri Ma <huri.ma@nyu.edu>
2018-04-21 19:33:15 -0400

    SequentialCPU half done

--- sim/ooo_cpu.cpp
+++ sim/ooo_cpu.cpp
@@ -1,26 +1,95 @@
 #include "ooo_cpu.h"
 
-//WriteBack,
-//InstExecution,
-//InstIssue,
-//InstDispatch,
-//InstFetch
+
+size_t SequentialCPU::get_operation_latency(const u32 opcode) const {
+  //todo get corresponding latency
+  return 1;
+}
+
+bool SequentialCPU::has_destination_memory(CPUEventData * event_data) const {
+  for (int i = 0; i < NUM_INSTR_DESTINATIONS; i++) {
+    if (event_data->destination_memory[i] != 0) return true;
+  }
+  return false;
+}
+
+bool SequentialCPU::has_source_memory(CPUEventData * event_data) const {
+  for (int i = 0; i < NUM_INSTR_SOURCES; i++) {
+    if (event_data->source_memory[i] != 0) return true;
+  }
+  return false;
+}
 
 void SequentialCPU::proc(u64 tick, EventDataBase* data, EventType type) {
   (void)tick;
-  CPUEventData *cpu_data = (CPUEventData *) data;
-  EventEngine *evnet_queue = EventEngineObj::get_instance();
-
+  auto *cpu_event_data = (CPUEventData *) data;
+  EventEngine *event_queue = EventEngineObj::get_instance();
   switch (type) {
     // Issue some MemoryOnAccess event & no need to wait for accessible
+    // How to ignore the on arrive
     case WriteBack : {
+#ifdef DEBUG
+      assert(! _execution_list.empty());
+      assert(_execution_list.front() == cpu_event_data);
+#endif
+      _execution_list.pop_front();
+      for (int i = 0; i < NUM_INSTR_DESTINATIONS; i++) {
+        if (cpu_event_data->destination_memory[i] != 0) {
+          MemoryAccessInfo acces_to_writeback(cpu_event_data->PC,
+                                              cpu_event_data->destination_memory[i]);
+              _memomry_connecter.issue_memory_access(acces_to_writeback);
+        }
+      }
     } break;
     // Get the opcode latency and Issue Writeback event if there are destination memory
-    case InstExecution : {} break;
-    // check ready state of all the operand of instruction and issue InstExecution
-    case InstIssue : {} break;
+    case InstExecution : {
+      if (_execution_list.empty()) {
+#ifdef DEBUG
+
+#endif
+        Event *e = new Event(InstIssue, this, nullptr);
+        event_queue->register_after_now(e, _issue_to_exec_latency, _priority);
+      }
+      if (has_destination_memory(cpu_event_data)) {
+        Event *e = new Event(WriteBack, this, cpu_event_data);
+        event_queue->register_after_now(e, get_operation_latency(cpu_event_data->opcode),
+                                        _priority);
+      } else {
+        _execution_list.pop_front();
+      }
+      Event *e = new Event(InstIssue, this, nullptr);
+      event_queue->register_after_now(e, get_operation_latency(cpu_event_data->opcode),
+                                      _priority);
+    } break;
+    case InstIssue : {
+      // check the issue queue of instructions and add execution event
+      if (! _issue_list.empty()) {
+        CPUEventData *to_exec_event = _issue_list.front();
+        _issue_list.pop_front();
+#ifdef DEBUG
+        assert(to_exec_event->ready);
+        assert(_execution_list.empty());
+#endif
+        _execution_list.push_back(to_exec_event);
+        Event *e = new Event(InstIssue, this, to_exec_event);
+        event_queue->register_after_now(e, _issue_to_exec_latency, _priority);
+      } else {
+        // no instruction ready, read from
+        Event *e = new Event(InstDispatch, this, nullptr);
+        event_queue->register_after_now(e, 0, _priority);
+      }
+
+
+    } break;
     // If the instruction list is not full read new instruction
-    case InstFetch : {} break
+    case InstDispatch : {
+      if (has_source_memory(cpu_event_data)) {
+        // check the pending status of the source memory and create InstExecution event
+      } else {}
+    } break;
+    case InstFetch: {
+
+    } break;
   }
 
 }
--- sim/ooo_cpu.h
+++ sim/ooo_cpu.h
@@ -10,42 +10,64 @@
 // 重复的address会被
 // poll
 
+
+
+
 struct CPUEventData : public EventDataBase {
-  u32 opcode;
-  u64 PC;
-  u64 destination_memory[NUM_INSTR_DESTINATIONS]; // output memory
-  u64 source_memory[NUM_INSTR_SOURCES];
+  u32  opcode;
+  u64  PC;
+  u64  destination_memory[NUM_INSTR_DESTINATIONS];
+  u64  source_memory[NUM_INSTR_SOURCES];
+  bool ready;
   CPUEventData(const TraceFormat &);
 };
 
-class MemoryConnecter {
-
-};
 
 class SequentialCPU : public EventHandler {
  private:
   const size_t             _id;
   const MultiTraceLoader * _multi_trace_loader;
   TraceFormat              _current_trace;
-<<<<<<< HEAD
-  const MemoryConnecter    _memomry_connecter;
-  list<TraceFormat>        _instruction_list;
-=======
-  MemoryConnecter    _memomry_connecter;
+  CpuConnector             _memomry_connecter;
 
-  unordered_set<u64>       _pending_refs;
->>>>>>> 68057ce40fd3e36552042ab983ce8a835e34120b
+  list<u64>                 _pending_refs;
 
-  size_t get_operation_latency(const TraceFormat& trace) const;
+  list<CPUEventData*>       _execution_list;
+  list<CPUEventData*>       _issue_list;
+  list<CPUEventData*>       _dispatch_list;
 
+
+  size_t get_operation_latency(const u32 opcode) const;
+  bool has_destination_memory(CPUEventData *) const;
+  bool has_source_memory(CPUEventData *) const;
+
+  const u8   _priority = 1;
+  const u8   _execution_list_limit = 1;
+  const u8   _issue_list_limit = 8;
+  const u8   _dispatch_list_limit = 16;
+
+  const u32  _issue_to_exec_latency = 1;
+  const u32  _dispath_to_issue_latency = 1;
+
+  inline bool execution_list_full() {
+    return _execution_list.size() >= _execution_list_limit;
+  }
+
+  inline bool issue_list_full() {
+    return _issue_list.size() >= _issue_list_limit;
+  }
+
+  inline bool dispatch_list_full() {
+    return _dispatch_list.size() >= _dispatch_list_limit;
+  }
 
  protected:
   void proc(u64 tick, EventDataBase* data, EventType type);
 
  public:
   // also initialize _memomry_connecter
-  SequentialCPU(size_t id, MultiTraceLoaderObj multi_trace_loader):
-      _id(id), _multi_trace_loader(multi_trace_loader.get_instance()) {}
+//  SequentialCPU(size_t id, MultiTraceLoaderObj multi_trace_loader):
+//      _id(id), _multi_trace_loader(multi_trace_loader.get_instance()) {}
 };
 
 
